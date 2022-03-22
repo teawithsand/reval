2d5026f01036f85207033cc4f05d208d45890d21
teawithsand <teawithsand@gmail.com>
2022-03-22 00:09:30 +0000

    Added testing for JSON FieldProcessor

---
+++ jsonutil/processor_test.go
@@ -0,0 +1,36 @@
+package jsonutil_test
+
+import (
+	"testing"
+
+	"github.com/teawithsand/reval"
+	"github.com/teawithsand/reval/jsonutil"
+	"github.com/teawithsand/reval/stdesc"
+)
+
+type A struct {
+	Field string `json:"field"`
+}
+
+func TestProcessor(t *testing.T) {
+	dw := reval.DefaultWrapper{
+		DescriptorComputer: &stdesc.Comptuer{
+			FieldProcessor: jsonutil.FieldProcesor,
+		},
+	}
+
+	assert := func(x bool, msg string) {
+		if t.Failed() {
+			return
+		}
+
+		if !x {
+			t.Error("assert filed", msg)
+		}
+	}
+
+	v := reval.MustWrap(dw.Wrap(A{})).(reval.KeyedValue)
+
+	assert(v.HasField("field"), "has json field")
+	assert(!v.HasField("Field"), "does not have go field")
+}
